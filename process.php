069fbca25ee81888273e27bbdd3e8b8e8ae219a1
omarqe <omar.veyron@gmail.com>
2017-05-11 13:27:39 +0800

    Save data into the patient's data file.

--- process.php
+++ process.php
@@ -42,15 +42,15 @@ switch( $action ){
 			);
 		}
 
+		if ( !save_data(array('worries' => array_keys($worry_answers))) )
+			send_response( "Sorry, but we can't save your data at the moment. Please try again.", "red" );
+
 		send_response( "", "", true, compact('worry_answers') );
 		exit;
 
 	case "begin":
-		$nickname = parse_arg( 'nickname', $_POST );
+		$nickname = ucwords( parse_arg('nickname', $_POST) );
 		$shake = 'input[name=nickname]';
-
-		if ( empty($nickname) )
-			send_response( u('nickname_pls'), 'yellow', false, compact('shake') );
 
 		$nick_split = explode(' ', $nickname);
 		if ( count($nick_split) > 1 ){
@@ -60,7 +60,23 @@ switch( $action ){
 				$nickname = substr( $nickname, 0, 15 );
 		}
 
+		if ( ! save_data(compact('nickname')) )
+			send_response( sprintf("We are sorry %s, but there's an error while processing your request. Please refresh and try again.", $nickname), "red", false );
+
 		send_response( "", "", true, compact('shake') );
+		exit;
+
+	case "choose_stage":
+		$stage = parse_arg( 'stage', $_GET );
+		if ( !is_numeric($stage) )
+			send_response( "Sorry, we cannot proceed since we can't verify your cancer stage.", "red", false );
+		elseif ( $stage <= 0 || $stage >= 3 )
+			send_response( "Sorry, this PDA is only available to patients with cancer stage 1 and 2.", "red", false );
+
+		if ( !save_data(array('cancer_stage' => (int)$stage)) )
+			send_response( "Sorry, but we can't save your data right now.", "red", false );
+
+		send_response( "", "", true );
 		exit;
 
 	case "concern":
@@ -75,6 +91,7 @@ switch( $action ){
 			'ready_for_treatment'
 		);
 
+
 		$postdata = parse_args( array_merge($inputarr, $required), $_POST );
 		$empties = array();
 		foreach ( $postdata as $key => $value ){
@@ -84,6 +101,30 @@ switch( $action ){
 
 		if ( !empty($empties) )
 			send_response( u("required_inputs_empty"), "red", false, compact('empties') );
+
+		// Support options
+		$support_options = array();
+		foreach ( ['support_option1', 'support_option2', 'support_option3', 'support_option4', 'support_option5'] as $support_key ){
+			$support_val = strtolower($postdata[$support_key]);
+			$support_options[$support_key] = ($support_val === 'yes');
+		}
+
+		// Build the data structure
+		$structure = array(
+			"prefers" => array(
+				"surgery" => $postdata['surgery_prefer_pos'],
+				"no_surgery" => $postdata['surgery_prefer_neg']
+			),
+			"concern" => $postdata['concerns'],
+			"support" => $support_options,
+			"ready" => $postdata['ready_for_decision'],
+			"preferred_treatment" => $postdata['ready_for_treatment']
+		);
+
+		//send_response(print_p($structure,true), 'green');
+
+		if ( !save_data(array('knowing_important' => $structure)) )
+			send_response( "Sorry, but we can't save your data right now.", "red", false );
 
 		send_response("", "", true);
 		exit;
